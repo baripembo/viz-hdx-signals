cb8292f710612738f789384ef7433bc0f2d61f3c
baripembo <erikawei@gmail.com>
2024-04-18 15:04:55 -0400

    init

--- src/App.svelte
+++ src/App.svelte
@@ -1,280 +1,117 @@
 <script>
   import * as d3 from 'd3';
+  import { sliderRight } from 'd3-simple-slider';
   import Papa from 'papaparse'
   import { onMount } from 'svelte';
   import KeyFigure from './lib/KeyFigure.svelte'
-  import Bar from './lib/Charts/Bar.svelte'
   import Map from './lib/Map.svelte'
-  import Pyramid from './lib/Charts/Pyramid.svelte'
 
-  const randomData = (seriesCount) => {
-    return d3.range(seriesCount).map(function (d) {
-      return {
-        date: d,
-        value: Math.random(d)*100
-      };
-    })
-  };
+  let dateSlider, dates, regions, regionSelect, indicators;
+  
+  $: signalsData = [];
 
-  let countryData = [];
-  Papa.parse('https://hapi-testing.innovation.humdata.org/api/location?output_format=csv&limit=1000&offset=0', {
+  Papa.parse('cholera_signals.csv', {
     header: true,
     download: true,
     complete: function(results) {
-      countryData = results.data
+      signalsData = results.data.filter(d => d.iso3 !== '');
+      dates = signalsData.map(d => new Date(d.date));
+      
+      let regionArray = signalsData.map(d => d.region);
+      regions = [... new Set(regionArray)];
+      
+      let indicatorArray = signalsData.map(d => d.indicator_name);
+      indicators = [... new Set(indicatorArray)];
+
+      console.log(signalsData)
+
+      createDateSlider();
     }
   })
 
-  // const url = 'https://stage.hapi-humdata-org.ahconu.org/api/admin1?location_code=afg&output_format=json&offset=0';
-  // let admin1Data = [];
-
-  //dummy pie data
-  let requirement = 90;
-  let funded = 15;
-  let fundedPercent = funded/requirement;
-  let pieData = [1-fundedPercent, fundedPercent];
-
-  //dummy key figures
-  let keyFigureData = [
-    {title: 'Key figure', value: '1,200,000', series: randomData(10)},
-    {title: 'Key figure', value: '300000', series: randomData(10)},
-    {title: 'Key figure', value: '1100000000', valueFormat:'$.2s'},
-    {title: 'Key figure', value: '1100', series: randomData(5), seriesType: 'column'},
-    {title: 'Key figure', value: '500'},
-    {title: 'Key figure', value: '500', series: pieData, seriesType: 'pie'},
-  ];
-
-  let tabs = [
-    {title: 'Population', id: 'population'},
-    {title: '3W', id: '3w'},
-    // {title: 'People in Need', id: ''},
-    // {title: 'Internally Displaced People', id: ''},
-    // {title: 'Refugees', id: ''},
-    // {title: 'Returnees', id: ''},
-    // {title: 'Organizations', id: ''},
-    // {title: 'Food Insecurity', id: ''},
-    // {title: 'Malnutrition', id: ''},
-    // {title: 'Health', id: ''},
-  ];
-
-  let sidebarWidth, scrollingWrapperHeight, scrollingWrapper;
-
-  let countrySelect = 'AFG'
-  $: selected = countryData.find((c) => {
-    return c.code === countrySelect 
-  });
-
-  //Total key figure
-  $: totalValue = 0;
-
-  $: rankingData = [];
-  $: ageData = [];
-  $: metadata = {};
-  $: currentLayer = 'population';
-
-  function getPopulationbyCountry(iso3) {
-    Papa.parse(`https://hapi-testing.innovation.humdata.org/api/themes/population?location_code=${iso3}&admin_level=1&output_format=csv&limit=1000&offset=0`, {
-      header: true,
-      download: true,
-      complete: function(results) {
-        if (results.data[0] !== undefined) {
-          let metadataURL = `https://hapi-testing.innovation.humdata.org/api/resource?hdx_id=${results.data[0].resource_hdx_id}&output_format=csv`;
-          getMetadata(metadataURL);
-        }
-
-        const ages = results.data.reduce((acc, { gender_code, age_range_code, population }) => {
-            //skip entries with empty gender_code or age_range_code
-            if (!gender_code.trim() || !age_range_code.trim()) {
-              return acc;
-            }
-
-            //create unique key for each gender/age combo
-            const key = `${gender_code}_${age_range_code}`;
-
-            //create accumulator if it doesnt exist dor this age/gender combo
-            if (!acc[key]) {
-              acc[key] = {
-                gender_code,
-                age_range_code,
-                total_population: 0
-              };
-            }
-
-            //get total pop for this age/gender combo
-            acc[key].total_population += +population;
-
-            return acc;
-        }, {});
-
-        //assign data for population pyramid
-        ageData = Object.values(ages);
-
-        //get population per adm area
-        let popByAdm = d3.rollup(results.data, v => d3.sum(v.filter(d => d.age_range_code === '' && d.gender_code === ''), d => d.population), d => d.admin1_name)
-        let arr = [];
-        popByAdm.forEach(function(value, key) {
-          if (key !== undefined)
-            arr.push({name: key, value: value});
-        })
-
-        //assign data for ranking chart
-        rankingData = arr;
-        totalValue = d3.sum(popByAdm.values());
-      }
-    })
-  }
-
-  function getOrgsbyCountry(iso3) {
-    Papa.parse(`https://hapi-testing.innovation.humdata.org/api/themes/3W?location_code=${iso3}&output_format=csv&offset=0`, {
-      header: true,
-      download: true,
-      complete: function(results) {
-        if (results.data[0] !== undefined) {
-          let metadataURL = `https://hapi-testing.innovation.humdata.org/api/resource?hdx_id=${results.data[0].resource_hdx_id}&output_format=csv`;
-          getMetadata(metadataURL);
-        }
-
-        //console.log(results.data)
-
-        let orgsByAdm = d3.rollup(results.data, v => v.length, d => d.admin1_name);
-        let arr = [];
-        orgsByAdm.forEach(function(value, key) {
-          if (key !== undefined)
-            arr.push({name: key, value: value});
-        })
-
-        rankingData = arr;
-        totalValue = d3.sum(orgsByAdm.values());
-      }
-    })
-  }
-
-
-  function getMetadata(url) {
-    Papa.parse(url, {
-      header: true,
-      download: true,
-      complete: function(results) {
-        metadata.date = results.data[0].update_date;
-        metadata.datasetURL = results.data[0].dataset_hdx_link;
-        metadata.provider = results.data[0].dataset_hdx_provider_name;
-      }
-    })
-  }
-
-  function getCountryData(iso3) {
-    if (currentLayer === '3w')
-      getOrgsbyCountry(iso3);
-    else
-      getPopulationbyCountry(iso3);
-  }
-
-  function onLayerChange(id) {
-    currentLayer = id;
-
-    if (currentLayer === '3w')
-      getOrgsbyCountry(countrySelect);
-    else
-      getPopulationbyCountry(countrySelect);
-  }
 
   onMount(async() => {
-    // const response = await fetch(url);
-    // admin1Data = await response.json();
-    // console.log(admin1Data)
-    getPopulationbyCountry(countrySelect);
-
-    //calculate available space for ranking chart
-    scrollingWrapperHeight = 400;//window.innerHeight - scrollingWrapper.getBoundingClientRect().top - 80;
-    scrollingWrapper.style.height = scrollingWrapperHeight + 'px';
   });
+
+  function createDateSlider() {
+    const sliderHeight = 680;
+    dates = dates.sort((a, b) => a.getTime() - b.getTime());
+
+    const slider = sliderRight()
+      .min(d3.min(dates))
+      .max(d3.max(dates))
+      .tickFormat(d3.timeFormat('%b %d, %Y'))
+      .step(1)
+      .tickValues(dates)
+      .marks(dates)
+      .default([dates[dates.length-1],dates[0]])
+      .width(150)
+      .height(sliderHeight-50)
+      .handle(d3.symbol().type(d3.symbolCircle).size(150)())
+      .fill('#007CE0');
+
+    const g = d3.select(dateSlider)
+      .append('svg')
+      .attr('width', 150)
+      .attr('height', sliderHeight)
+      .append('g')
+      .attr('transform', 'translate(25,25)');
+
+    g.call(slider);
+  }
 </script>
 
 <main>
-  <div class='select-wrapper'>
-    <select bind:value={countrySelect} on:change={() => getCountryData(countrySelect)}>
-      {#each countryData as {code, name}}
-        {#if code!=''}
-          <option value={code}>{name}</option>
-        {/if}
-      {/each}
-    </select>
-  </div> 
-
-
-<!--   {#if selected!=undefined}
-    <h1>{selected.name}</h1>
-  {/if} -->
-  
-  <h2 class='header'>Header</h2>
-  <div class='grid-container key-figure-container'>
-    {#each keyFigureData as keyFigure}
-      <div class='col-2'>
-        <KeyFigure {...keyFigure} />
+  <div class='filters'>
+    {#if regions}
+      <div class='select-wrapper'>
+        <select bind:value={regionSelect}><!--on:change={() => getCountryData(countrySelect)}-->
+          <option value={'All Regions'}>{'All Regions'}</option>
+          {#each regions as region}
+            <option value={region}>{region}</option>
+          {/each}
+        </select>
       </div>
-    {/each}
-  </div>
+    {/if}
 
-  <h2 class='header'>Header</h2>
-
-  <div class='tabs'>
-    {#each tabs as {title, id}, i}
-      <div class='tab' class:active={id === currentLayer} on:click={() => onLayerChange(id)}><a href='#' {title}>{title}</a></div>
-    {/each}
-  </div>
-
-<!--   <div class='select-wrapper'>
-    <select>
-      {#each tabs as {title, id}}
-        <option value={id}>{title}</option>
-      {/each}
-    </select>
-  </div> -->
-
-  <div class='content grid-container'>
-    <div class='sidebar col-5' bind:clientWidth={sidebarWidth}>
-      <KeyFigure title={'Total'} value={totalValue} metadata={metadata} /><!-- series={randomData(10)} -->
-
-      <h3 class='chart-title'>Ranking (Top 10)</h3>
-      <div class='scrolling-wrapper' bind:this={scrollingWrapper}>
-        {#if sidebarWidth>0}
-          <div class='ranking-container'>
-            <Bar data={rankingData} width={sidebarWidth} />
-          </div>
-          {#if currentLayer==='population'}
-            <Pyramid data={ageData} title={'Population Pyramid'} width={sidebarWidth} />
-          {/if}
-        {/if}
+    {#if indicators}
+      <div class='select-wrapper'>
+        <select>
+          <option value={'All Indicators'}>{'All Indicators'}</option>
+          {#each indicators as indicator}
+            <option value={indicator}>{indicator}</option>
+          {/each}
+        </select>
       </div>
+    {/if}
 
-    </div>
-    <div class='main-content col-7'>
-      <Map center={[67, 34]} zoom={5} />
-    </div>
+    <input type='checkbox' id="onlyHRP"> Only HRP
+  </div>
+  <div class='slider-container'>
+    <div class='slider' bind:this={dateSlider} />
+  </div>
+  <div class='map'>
+    <Map {signalsData} />
   </div>
 </main>
 
 <style lang='scss'>
-  .key-figure-container {
-    padding-left: 15px;
+  main {
+    position: relative;
   }
-  .main-content  {
-    margin: 0;
+  .filters {
+    align-items: baseline;
+    display: flex;
   }
-  .sidebar {
-    margin-right: 0;
-    padding-top: 15px;
+  .slider-container {
+    background-color: rgba(255,255,255,0.7);
+    height: 100%;
+    position: absolute;
+    width: 150px;
+    z-index: 3;
   }
-  .chart-title {
-    margin-top: 15px;
-  }
-  .scrolling-wrapper {
-    overflow-y: scroll;
-  }
-  .select-wrapper {
-    margin-top: 20px;
-  }
-  select {
-    font-size: 20px;
+  .axis text,
+  .slider text {
+    font-size: 18px;
   }
 </style>
--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -1,24 +1,28 @@
 <script>
 	import mapboxgl from 'mapbox-gl';
 	import * as d3 from 'd3';
+	import { legendColor } from 'd3-svg-legend';
 	import { onMount, onDestroy } from 'svelte';
 	import '../../node_modules/mapbox-gl/dist/mapbox-gl.css'
 
 	mapboxgl.baseApiUrl = 'https://data.humdata.org/mapbox';
 	mapboxgl.accessToken = 'cacheToken';
 
-	export let center = [67, 33];
-	export let zoom = 4.8;
+	export let signalsData;
+	export let center = [20, 10];
+	export let zoom = 2;
 
-	let map, mapContainer, geojson;
+	let map, mapContainer, signalsGeoData, legend, colorScale, hoverTimer;
 
-	let colorRange = ['#CCE5F9','#99C4E7','#66A4D6','#3383C4','#0063B3'];
+	let colorRange = ['#C25048','#F2645A','#F7A29C','#FCE0DE'];
 	let tooltip = d3.select('.tooltip');
 	let numFormat = d3.format(',');
+	let dateFormat = d3.utcFormat('%b %d, %Y');
+
 
 	onMount(() => {
 		//calculate available space for map
-		let mapHeight = 600;//window.innerHeight - mapContainer.getBoundingClientRect().top - 80;
+		let mapHeight = 700;
 		mapContainer.style.height = mapHeight + 'px';
 
 		//init map
@@ -30,56 +34,141 @@
 	  });
 
 	  map.addControl(new mapboxgl.NavigationControl({showCompass: false}))
-	    .addControl(new mapboxgl.AttributionControl(), 'bottom-left');
+	     .addControl(new mapboxgl.AttributionControl(), 'bottom-left');
 
 	  tooltip = new mapboxgl.Popup({
-			closeButton: false,
-			closeOnClick: false,
+			closeButton: true,
+			closeOnClick: true,
+			closeOnMove: true,
 			className: 'map-tooltip'
 		});
 
 	  map.on('load', function() {
 	    console.log('Map loaded')
+		  console.log('in map', signalsData)
+			
+		  //set alert level color scale
+			colorScale = d3.scaleOrdinal()
+      .domain(['High concern', 'Medium concern', 'Low concern'])
+      .range(colorRange);
+
+		  //initPolys();
+		  loadFeatures();
+		  createLegend();
 	  });
 
-	  loadFeatures();
+
 	});
 
-	async function loadFeatures() {
-		const response = await fetch('updated_data.geojson', {
-			body: JSON.stringify()
+
+	function createLegend() {
+		const colorLegend = legendColor()
+      .shape('path', d3.symbol().type(d3.symbolCircle).size(120)())
+			.scale(colorScale)
+		
+		d3.select(legend)
+			.call(colorLegend);
+	}
+
+	// function initPolys() {
+	//   //data join
+	//   var expression = ['match', ['get', 'ISO3_CODE']];
+	//   signalsData.forEach(function(d, i) {
+	//   	if (d.value!==undefined && i<3) {
+	// 	    var val = +d.value;
+	// 	    var color = '#888888';
+	// 	    expression.push(d.iso3, color);
+	//   	}
+	//   });
+
+	//   //default value for no data
+	//   expression.push('#EEEEEE');
+	  
+	//   //set properties
+	//   map.setPaintProperty('wrl polbnda 1m', 'fill-color', expression);
+	// }
+
+	function loadFeatures() {
+		let signals = [];
+		let hrpList = [];
+		signalsData.forEach(function(signal, i) {
+			if (signal.iso3 !== '') {
+				signals.push({
+					'type': 'Feature',
+					'geometry': {
+						'type': 'Point',
+						'coordinates': [signal.lon, signal.lat]
+					},
+					'properties': signal
+				});
+
+				//save list of hrps
+				if (signal.hrp_country == 'TRUE') hrpList.push(signal.iso3);
+			}
 		});
-	  geojson = await response.json();
 
-  	const max = d3.max(geojson.features, function(d) { return +d.properties['population_f_80+']; });
-  	const colorScale = d3.scaleQuantize().domain([0, max]).range(colorRange);
-
-	  geojson.features.forEach(function(f) {
-	    let prop = f.properties;
-	    prop['color'] = colorScale(+prop['population_f_80+'])
-	  });
-
-	  map.addSource('population-data', {
+		signalsGeoData = {
+			'type': 'FeatureCollection',
+			'features': signals
+		}
+		map.addSource('signals-source', {
 	    type: 'geojson',
-	    data: geojson
+	    data: signalsGeoData,
+	    generateId: true 
 	  });
 
+
+		//create color scale for markers
+		let indicatorColorScale = [
+	    'match',
+	    ['get', 'alert_level'],
+	    'High concern',
+	    colorRange[0],
+	    'Medium concern',
+	    colorRange[1],
+	    'Low concern',
+	    colorRange[2],
+	    /* other */ '#ccc'
+	  ];
+
+		//add signal markers
 	  map.addLayer({
-	    id: 'population-layer',
-	    source: 'population-data',
-	    type: 'fill',
+	    id: 'signal-dots',
+	    type: 'circle',
+	    source: 'signals-source',
 	    paint: {
-      	'fill-color': ['get', 'color'],
-	      'fill-outline-color': '#E0E0E0'
+	      'circle-color': indicatorColorScale,
+	      'circle-stroke-color': indicatorColorScale,
+	      'circle-opacity': 0.6,
+	      'circle-radius': 6,
+	      'circle-stroke-width': 1,
 	    }
 	  });
 
-	   //mouse events
-	  map.on('mouseenter', 'population-layer', onMouseEnter);
-	  map.on('mouseleave', 'population-layer', onMouseLeave);
-	  map.on('mousemove', 'population-layer', function(e) {
+	  // //highlight hrp countries
+	  // let expression = ['match', ['get', 'ISO3_CODE']];
+	  // let hrps = [... new Set(hrpList)]
+	  // hrps.forEach(function(d) {
+		//    expression.push(d, '#CCE5F9');
+	  // });
+	  // expression.push('#EEEEEE');
+	  // map.setPaintProperty('wrl polbnda 1m', 'fill-color', expression);
+
+	  //mouse events
+	  map.on('mouseenter', 'signal-dots', onMouseEnter);
+	  map.on('mouseleave', 'signal-dots', onMouseLeave);
+	  map.on('mousemove', 'signal-dots', function(e) {
 	    map.getCanvas().style.cursor = 'pointer';
-	    const content = `<h2>${e.features[0].properties.ADM1_EN}, ${e.features[0].properties.ADM0_EN}</h2>Population of Females 80+:<div class="stat">${numFormat(e.features[0].properties['population_f_80+'])}</div>`;
+	    let prop = e.features[0].properties;
+	    let alertDate = dateFormat(new Date(prop.date));
+	    let content = `<h2>${prop.country}</h2>`;
+	    content += `${alertDate}<br>`;
+	    content += `<div class="alert-table"><div>Indicator: <span class="stat">${prop.indicator_name}</span></div>`;
+	    content += `<div>Alert Level: ${prop.alert_level}</div></div>`;
+	    // content += `Value: <span class="stat">${numFormat(prop.value)}</span><br>`;
+	    // content += `Source: <span class="stat"><a href='${prop.source_url}' target='_blank'>${prop.indicator_source}</a></span><br>`;
+	    content += `<img class="plot" src="${prop.plot_url}" />`;
+	    content += `${prop.further_information}`;
 	    tooltip.setHTML(content);
 	    tooltip
 	      .addTo(map)
@@ -89,12 +178,17 @@
 
 	//mouse event/leave events
 	function onMouseEnter(e) {
+		clearTimeout(hoverTimer);
 	  map.getCanvas().style.cursor = 'pointer';
 	  tooltip.addTo(map);
+	  console.log(d3.select('.mapboxgl-popup-content'));
+	  console.log(d3.select('.map-tooltip'));
 	}
 	function onMouseLeave(e) {
-	  map.getCanvas().style.cursor = '';
-	  tooltip.remove();
+		// hoverTimer = setTimeout(() => {
+		//   map.getCanvas().style.cursor = '';
+		//   tooltip.remove();
+		// }, 2000);
 	}
 
 	onDestroy(() => {
@@ -104,9 +198,26 @@
 
 
 <div bind:this={mapContainer} />
+<div class='legend-container'>
+	<h4>Map Legend</h4>
+	<svg>
+		<g bind:this={legend} />
+	</svg>
+</div>
 
 <style lang='scss'>
-	div {
-		margin-bottom: 20px;
+	.legend-container {
+		background-color: rgba(255,255,255,0.7);
+		bottom: 20px;
+		font-size: 14px;
+		height: 150px;
+		position: absolute;
+		padding: 15px;
+		right: 20px;
+		width: 200px;
+		z-index: 2;
+		svg g {
+			transform: translate(7px, 7px);
+		}
 	}
 </style>
