518583b39f81f6b73c45f951f63d41efde0e416a
baripembo <erikawei@gmail.com>
2024-06-12 15:31:12 -0400

    add basic mp and gtm tracking

--- src/App.svelte
+++ src/App.svelte
@@ -263,8 +263,21 @@
     }
   }
 
+
+  function initTracking() {
+    //initialize mixpanel
+    var MIXPANEL_TOKEN = window.location.hostname=='data.humdata.org'? '5cbf12bc9984628fb2c55a49daf32e74' : '99035923ee0a67880e6c05ab92b6cbc0';
+    mixpanel.init(MIXPANEL_TOKEN);
+    mixpanel.track('page view', {
+      'page title': document.title,
+      'page type': 'datavis'
+    });
+  }
+
   onMount(() => {
     d3.select('.filter-list').node().style.height = (map.isMobile()) ? ((window.innerHeight - headerHeight)*0.4) + 'px' : window.innerHeight - (d3.select('header').node().getBoundingClientRect().height) + 'px';
+
+    initTracking();
   });
 
 </script>
